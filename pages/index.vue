3e042ca0a22123d8ef6d996ea03242c2379dc646
pub <pubing@go2.cn>
2024-03-15 16:05:21 +0800

    feat: main

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <a-skeleton />
+  <div class="">
+    <div class=" bg-slate-50 w-full float-left">
+      123
+      <a-skeleton />
+    </div>
+    <div class="bg-gray-100 hidden lg:block w-[295px] float-left">
+123
+    </div>
   </div>
 </template>
